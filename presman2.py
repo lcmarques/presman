085f4d42c8ac0ed9cf19abba2652671b46c22599
Luís Marques <lcarapinha@gmail.com>
2014-11-27 18:08:35 +0000

    initial version 1.0
    Some light changes. Only supports 12c now

--- presman2.py
+++ presman2.py
@@ -24,6 +24,17 @@ def headerText():
 	version = '1.0'
 	print 'pResman ' + version +' - Oracle Resource Manager Monitor  - Luis Marques (http://lcmarques.com)'	
 
+def headerCPU(refresh_rate):
+	HEADER = '\033[94m'
+	ENDC = '\033[0m'
+	print HEADER + 'CPU Information by Consumer Group '+ENDC+' (Refresh time: '+str(refresh_rate)+' seconds)\n'
+
+def headerSessionIO(refresh_rate):
+	HEADER = '\033[94m'
+	ENDC = '\033[0m'
+	print HEADER + 'Session I/O Information by Consumer Group '+ENDC+'(Refresh time: '+str(refresh_rate)+' seconds)\n'
+
+
 def ConfigSectionMap(section):
 	dict1 = {}
 	Config = ConfigParser.ConfigParser()
@@ -88,30 +99,25 @@ def runStatement(con, sql_text):
 	sql = cursor.execute(sql_text)
 	return cursor	
 
-def runStatementFetch(con, sql_text):
-	cursor=con.cursor()
-        sql = cursor.execute(sql_text)
-        result = cursor.fetchall()
-        #con.close()
-        return result
 
 def resman_perf():
 	sql_text = 'WITH sumcpu as (SELECT SUM(consumed_cpu_time) consumed_cpu_percent from v$rsrc_consumer_group where name not like \'ORA$%\')  SELECT name, active_sessions, execution_waiters, requests, cpu_wait_time, cpu_waits, consumed_cpu_time, round((consumed_cpu_time*100 / (select consumed_cpu_percent from sumcpu)), 2) consumed_cpu_percent, yields FROM v$rsrc_consumer_group where name not like \'ORA$%\''
 	return sql_text
 
 def resman_sess_io():
-	sql_text = 'WITH sessIO as (SELECT current_consumer_group, current_small_read_megabytes+current_large_read_megabytes+current_small_write_megabytes+current_large_write_megabytes sessions_io_total from v$RSRC_SESSION_INFO where current_consumer_group not like \'ORA$%\') select current_consumer_group NAME, sum(current_IO_service_time) IO_SERVICE_TIME, sum(current_small_read_megabytes)  SM_READ_MB, sum(current_large_read_megabytes) LM_READ_MB, sum(current_small_write_megabytes) SM_WRITE_MB, sum(current_large_write_megabytes) LM_WRITE_MB, round(((select SUM(sessions_io_total) from sessIO where current_consumer_group = a.current_consumer_group) / (select SUM(sessions_io_total) from sessIO))*100, 2) TOTAL_IO_PCT, SUM(current_undo_consumption) UNDO_CONSUMPTION FROM v$RSRC_SESSION_INFO a where current_consumer_group not like \'ORA$%\' group by current_consumer_group'	
-	return sql_text
+	sql_text = '''
+	WITH sessIO as (SELECT current_consumer_group, current_small_read_megabytes+current_large_read_megabytes+current_small_write_megabytes+current_large_write_megabytes sessions_io_total from v$RSRC_SESSION_INFO where current_consumer_group not like \'ORA$%\') 
+	select current_consumer_group NAME, sum(current_IO_service_time) IO_SERVICE_TIME, sum(current_small_read_megabytes)  SM_READ_MB, sum(current_large_read_megabytes) LM_READ_MB, 
+	sum(current_small_write_megabytes) SM_WRITE_MB, sum(current_large_write_megabytes) LM_WRITE_MB, round(((select SUM(sessions_io_total) 
+		from sessIO where current_consumer_group = a.current_consumer_group) / (select SUM(sessions_io_total) from sessIO))*100, 2) TOTAL_IO_PCT, SUM(current_undo_consumption) UNDO_CONSUMPTION FROM v$RSRC_SESSION_INFO a where current_consumer_group not like \'ORA$%\' group by current_consumer_group'''
 
-def resman_cpu_total():
-	sql_text = 'SELECT SUM(consumed_cpu_time) consumed_cpu_percent from v$rsrc_consumer_group where name not like \'ORA$%\''
 	return sql_text
-
 
 
 
 
 def resman_cpu_calculate(con):
+
 	cursor=runStatement(con, resman_perf())
 	result1=cursor.fetchall()
 	col_names = []
@@ -125,14 +131,13 @@ def resman_cpu_calculate(con):
 		 rows_char[j[0]]=j[7]
 
 	print x
-	print '--> CPU Consumed by Consumer Group Graph\n'
-
 	for y in rows_char:
 		value_chart=round(rows_char[y], 1)
 		print "{0:30} [{1:5}%]".format(y, str(value_chart)), int(round(value_chart,0))*'#'
-
+	print ''
 
 def resman_sess_io_calculate(con):
+
 	cursor=runStatement(con, resman_sess_io())
 	result1=cursor.fetchall()
 	col_names = []
@@ -147,12 +152,10 @@ def resman_sess_io_calculate(con):
 		rows_char[j[0]]=j[6]
 
 	print x
-	print '--> Session I/O Consumed by Consumer Group Graph\n'
-
 	for y in rows_char:
 		value_chart=round(rows_char[y], 1)
 		print "{0:30} [{1:5} %]".format(y, str(value_chart)), int(round(value_chart,0))*'#'
-
+	print ''
 
 
 
@@ -172,25 +175,20 @@ def showMyScreen():
 		con = connectDB(connection_string)
 		while 1:
 			# only works for UNIX shells
-
 			time.sleep(refresh_rate)
-			os.system('clear')
-			headerText()
-			print options_all
-			if ('session_io' in options_all) and  ('cpu' in options_all):
-				resman_sess_io_calculate(con)
-				resman_cpu_calculate(con)
 
-			#if 'cpu' in options_all:
-			#	resman_cpu_calculate(con)
-    		
-    		
-    			print 'FODASSE'
-    			
-			
+			os.system('cls' if os.name == 'nt' else 'clear')
+			headerText()
+			if ('session_io' in options_all) and  ('cpu' in options_all):
+				headerCPU(refresh_rate)
+				resman_cpu_calculate(con)
+				headerSessionIO(refresh_rate)
+				resman_sess_io_calculate(con)
+
 
 	except KeyboardInterrupt:
 		print("Ok ok, quitting")
+		con.close()
 		sys.exit(1)
 
 
